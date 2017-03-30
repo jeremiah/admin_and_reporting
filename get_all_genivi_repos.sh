ea0fc231c73120b7309a94da775a35518f7f03fe
Jeremiah C. Foster <jeremiah@jeremiahfoster.com>
2017-03-30 10:36:55 -0700

    Comments on API usage.

--- get_all_genivi_repos.sh
+++ get_all_genivi_repos.sh
@@ -4,5 +4,6 @@
 # account under "settings."
 TOKEN=
 
-# Get all GENIVI repos.
+# Get all GENIVI repos. You must have a token to access the API which
+# is also rate limited. Note also the pagination.
 curl -H "Authorization: token $TOKEN" https://api.github.com/orgs/genivi/repos?per_page=100 -o foo
