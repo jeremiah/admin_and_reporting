b71df62dbaadce7be8076dd08093610fbbef1dee
Jeremiah C. Foster <jeremiah@jeremiahfoster.com>
2017-04-17 11:19:23 -0700

    Added code header to shell script.

--- get_all_genivi_repos.sh
+++ get_all_genivi_repos.sh
@@ -1,5 +1,17 @@
 #!bin/bash
 
+# SPDX license identifier: MPL-2.0
+
+# Copyright (C) 2017, Jeremiah C. Foster <jfoster@luxoft.com>
+
+# This file is part of the GENIVI admin and reporting directory
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License (MPL), v. 2.0.  If a copy of the MPL was not distributed with
+# this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# For further information see http://www.genivi.org/.
+
 # This is the GENIVI token that is created in the personal GitHub
 # account under "settings."
 TOKEN=
