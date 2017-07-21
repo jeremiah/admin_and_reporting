d9bcdbe4fc5189a140e2701f28703627edaec0a4
Jeremiah C. Foster <jfoster@luxoft.com>
2017-07-20 19:19:33 -0700

    get_all_genivi_repos.sh: Now with error message. Also a perl hack to 			 create a back-up file and munge the incoming 			 file a bit.

--- get_all_genivi_repos.sh
+++ get_all_genivi_repos.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# SPDX license identifier: MPL-2.0
 
+# SPDX-license-identifier: MPL-2.0
 # Copyright (C) 2017, Jeremiah C. Foster <jfoster@luxoft.com>
-
 # This file is part of the GENIVI admin and reporting directory
 
 # This Source Code Form is subject to the terms of the Mozilla Public
@@ -12,10 +11,17 @@
 
 # For further information see http://www.genivi.org/.
 
-# This is the GENIVI token that is created in the personal GitHub
-# account under "settings."
+# The TOKEN var is the GENIVI token that is created in your GitHub
+# account under "settings." 
 TOKEN=
 
-# Get all GENIVI repos. You must have a token to access the API which
-# is also rate limited. Note also the pagination.
-curl -H "Authorization: token $TOKEN" https://api.github.com/orgs/genivi/repos?per_page=100 -o genivi-repos.json
+# Get all GENIVI repos. Note you must have the token for the api and the use of pagination.
+# The -H flag is to send the authorization header
+if curl --silent -H "Authorization: token $TOKEN" https://api.github.com/orgs/genivi/repos?per_page=100 -o genivi-repos.js; then
+    # This is nasty, but there is just one "[" in the file
+    # since it is a JS array so it shouldn't be a problem ;)
+    # This ensures we have a proper JS array in the file declared as a var
+    perl -pi'.bak' -e 's/\[/var repos = \[/' genivi-repos.js
+else
+    echo "Problem accessing the GitHub API."
+fi
